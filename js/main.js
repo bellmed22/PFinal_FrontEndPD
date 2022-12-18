c46b94329b443ba26d45d8e886757fec898457ca
Belén LM <113479814+bellmed22@users.noreply.github.com>
2022-12-18 20:37:55 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
 
-// SELECTORES
-
-const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-        return [...document.querySelectorAll(el)]
-    } else {
-        return document.querySelector(el)
-    }
-}
-
-const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-        if (all) {
-            selectEl.forEach(e => e.addEventListener(type, listener))
-        } else {
-            selectEl.addEventListener(type, listener)
-        }
-    }
-}
-
-const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-}
 
 
 // SLIDER_HEADER
@@ -161,7 +136,31 @@ const onscroll = (el, listener) => {
 
 
 
+// SELECTORES
 
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+            selectEl.addEventListener(type, listener)
+        }
+    }
+}
+
+const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+}
 
 
 
@@ -223,6 +222,22 @@ const onscroll = (el, listener) => {
 
 
     //SCROLL
+
+    //Toggle .header-scrolled class --> #header
+
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
 
     //Mobile nav toggle
 
